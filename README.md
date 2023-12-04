1e80a67e31563b25d188072553c3caaeff2cf889
choorywey <123738595+choorywey@users.noreply.github.com>
2023-12-04 17:21:07 +0100

    feat(main.js): class register add and update

---
+++ README.md
@@ -0,0 +1,63 @@
+# README
+
+## Descrizione
+Questo script implementa un'applicazione web per gestire una lista di persone. Gli utenti possono eseguire operazioni CRUD (Create, Read, Update, Delete) per gestire le informazioni sulla lista.
+https://chiaraferrara.github.io/Rubrica-Crud-JS/
+## Operazioni CRUD
+
+### Creazione di una Persona
+1. Inserire il nome nella casella di input con l'ID `name-input-field`.
+2. Inserire il cognome nella casella di input con l'ID `surname-input-field`.
+3. Fare clic sul pulsante con l'ID `add-button`.
+4. Una nuova persona sarà aggiunta alla lista nella sezione con la classe `people-container`.
+
+### Lettura di una Persona
+1. La lista di persone è visualizzata nella sezione con la classe `people-container`.
+2. Ogni persona è rappresentata con nome, cognome e pulsanti per le operazioni di modifica ed eliminazione.
+
+### Aggiornamento di una Persona
+1. Fare clic sul pulsante "Modifica" (icona matita) accanto alla persona che si desidera modificare.
+2. Nella sezione di modifica, inserire i nuovi valori per nome e cognome.
+3. Fare clic sul pulsante "Salva" per applicare le modifiche.
+4. Le informazioni sulla persona saranno aggiornate nella lista.
+
+### Eliminazione di una Persona
+1. Fare clic sul pulsante "Elimina" (icona cestino) accanto alla persona che si desidera eliminare.
+2. La persona sarà rimossa dalla lista.
+
+### Note Aggiuntive
+- È possibile modificare una persona alla volta.
+- Il pulsante "Modifica" nella sezione di una persona sarà disabilitato durante la modifica di un'altra persona.
+- Assicurarsi di inserire valori validi per nome e cognome prima di aggiungere una persona.
+
+## Struttura del Codice
+Il codice è organizzato in una classe `Person` che rappresenta un individuo con un ID, nome e cognome. Le interazioni con l'interfaccia utente sono gestite attraverso eventi e funzioni di manipolazione del DOM.
+
+### File e Risorse
+- `index.html`: Contiene la struttura HTML dell'applicazione.
+- `style.css`: Definisce lo stile dell'applicazione.
+- `main.js`: Contiene il codice JavaScript per la logica dell'applicazione.
+
+## Descrizione del CRUD dell'Applicazione
+
+### Creazione (Create)
+- Quando un utente clicca sul pulsante "Aggiungi", i valori inseriti nei campi `name-input-field` e `surname-input-field` vengono utilizzati per creare una nuova istanza di `Person`.
+- Questa istanza viene quindi aggiunta alla lista visibile nella sezione `people-container` e alla mappa `peopleMap`.
+
+### Lettura (Read)
+- La lista di persone viene visualizzata nella sezione `people-container`.
+- Ogni persona mostra il suo nome, cognome e pulsanti per le operazioni di modifica ed eliminazione.
+- La mappa `peopleMap` mantiene una traccia delle persone aggiunte con i loro ID associati.
+
+### Aggiornamento (Update)
+- Cliccando sul pulsante "Modifica" di una persona, si attiva la modalità di modifica per quella specifica persona.
+- Nella sezione di modifica, è possibile inserire nuovi valori per il nome e il cognome.
+- Cliccando sul pulsante "Salva", le informazioni della persona vengono aggiornate sia nella lista che nella mappa `peopleMap`.
+
+### Eliminazione (Delete)
+- Cliccando sul pulsante "Elimina" di una persona, si rimuove l'elemento dalla sezione `people-container`.
+- La persona corrispondente viene anche eliminata dalla mappa `peopleMap`.
+
+## Dipendenze
+L'applicazione non richiede dipendenze esterne. Si basa su HTML, CSS e JavaScript standard.
+
---
+++ main.js
@@ -0,0 +1,190 @@
+class RegistroClasse {
+    constructor() {
+        this.studenti = [];
+
+    }
+
+    aggiungiStudente(id, nome, cognome) {
+        const studente = {
+            id: id,
+            nome: nome,
+            cognome: cognome,
+            voti: [],
+        }
+        this.studenti.push(studente);
+        console.log(this.studenti)
+        return studente;
+    }
+
+    modificaInfo(id, nuovoNome, nuovoCognome) {
+        const indice = this.studenti.findIndex(studente => studente.id == id);
+        this.studenti[indice].nome = nuovoNome;
+        this.studenti[indice].cognome = nuovoCognome;
+        console.log(this.studenti)
+    }
+}
+
+const nameinput = document.querySelector('#name-input-field');
+const surnameinput = document.querySelector('#surname-input-field');
+const addButton = document.querySelector('#add-button');
+const container = document.querySelector('.people-container');
+const editarea = document.querySelector('.edit-container');
+
+const registro = new RegistroClasse(); //oggetto registro instanziato
+
+var countid = 1;
+var click = false;
+
+// metodo callback. una volta cliccato aggiungerà dal text-input
+addButton.addEventListener('click', () => {
+    const nameInput = document.getElementById('name-input-field');
+    const surnameInput = document.getElementById('surname-input-field');
+
+    if (!nameInput || !surnameInput || nameInput.value.trim() === '' || surnameInput.value.trim() === '') {
+        return;
+    } else {
+        const name = nameInput.value;
+        const surname = surnameInput.value;
+        const id = countid++;
+
+
+
+
+
+        const personContainer = document.createElement('div');
+        personContainer.classList.add('person-item-container');
+        container.appendChild(personContainer);
+        // id. Nome Cognome
+        const paragraphId = document.createElement('p');
+        paragraphId.id = 'person-id';
+        paragraphId.innerText = `${id}`;
+        personContainer.appendChild(paragraphId);
+
+        const paragraphName = document.createElement('p');
+        paragraphName.id = 'person-name';
+        paragraphName.innerText = `${name} `;
+        personContainer.appendChild(paragraphName);
+
+
+        const paragraphSurname = document.createElement('p');
+        paragraphSurname.id = 'person-surname';
+        paragraphSurname.innerText = `${surname}`;
+        personContainer.appendChild(paragraphSurname);
+
+        //metodo aggiungiStudente della classe
+        registro.aggiungiStudente(id, name, surname);
+
+
+        //bottone edit
+        const editButton = document.createElement('button');
+        editButton.id = 'edit-info';
+        const editimg = document.createElement('img');
+        editimg.src = 'edit.svg'
+        editButton.appendChild(editimg);
+        personContainer.appendChild(editButton); //fa apparire il bottone edit all'interno di people-container.
+
+        //se clicco devo modificare le info 
+
+        editButton.addEventListener('click', () => {
+            click = !click;
+
+            if (click == false) {
+                editarea.innerHTML = ''; //questo fa in modo che ci sia un solo edit alla volta.
+                document.getElementById(editButton).disabled = true;
+            }
+
+            const editedStudent = registro.studenti.find(student => student.id === id); //trovo l'id dello studente per risalire a nome e cognome
+            const datiCorrenti = document.createElement('p');
+            datiCorrenti.innerText = `Stai modificando: ${editedStudent.nome} ${editedStudent.cognome}`;
+
+
+            const editContainer = document.createElement('div');
+            editContainer.classList.add('edit-info-container');
+
+            const newNameInput = document.createElement('input');
+            newNameInput.type = 'text';
+            newNameInput.value = name;
+            newNameInput.placeholder = 'Nuovo Nome';
+            newNameInput.id = 'text-input-field';
+
+
+
+            const newSurnameInput = document.createElement('input');
+            newSurnameInput.type = 'text';
+            newSurnameInput.value = surname;
+            newSurnameInput.placeholder = 'Nuovo Cognome';
+            newSurnameInput.id = 'text-input-field';
+
+
+            const saveButton = document.createElement('button');
+
+            const saveimg = document.createElement('img');
+            saveimg.src = 'save.svg'
+            saveButton.appendChild(saveimg);
+            saveButton.addEventListener('click', () => {
+                click = false;
+                const editedStudent = registro.studenti.find(student => student.id === id);
+                if (editedStudent) {
+                    // questio aggiorna il nome e cognome con dei nuovi valori
+                    editedStudent.nome = newNameInput.value;
+                    editedStudent.cognome = newSurnameInput.value;
+
+
+                    //metodo modificaInfo della classe
+                    registro.modificaInfo(id, editedStudent.nome, editedStudent.cognome)
+
+
+                    // aggiorniamo i dati
+                    paragraphName.innerText = `${editedStudent.nome} `;
+                    paragraphSurname.innerText = `${editedStudent.cognome}`;
+                    console.log('I nuovi dati:', newNameInput.value, newSurnameInput.value);
+                    editarea.innerHTML = ''; //non si vedrà più nulla dopo aver salvato
+
+
+                }
+            });
+
+
+            editContainer.appendChild(datiCorrenti); //voglio che visualizzi i dati precedenti...
+            editContainer.appendChild(newNameInput);
+            editContainer.appendChild(newSurnameInput);
+
+
+            editarea.appendChild(editContainer);
+            editContainer.appendChild(saveButton);
+
+
+
+        })
+
+        //bottone delete
+        const deleteBtn = document.createElement('button');
+        deleteBtn.id = 'delete-btn';
+        const deleteimg = document.createElement('img');
+        deleteimg.src = 'delete.svg'
+        deleteBtn.appendChild(deleteimg);
+        personContainer.appendChild(deleteBtn);
+
+        //se clicco devo eliminare la persona!!
+
+        //se clicco devo eliminare la persona
+        deleteBtn.addEventListener('click', () => {
+            if (click == false) {
+                // rimuove l'elemento html
+                container.removeChild(personContainer);
+
+                // rimuove la persona dalla mappa
+                delete peopleMap[id];
+                console.log(peopleMap);
+            } else {
+                return;
+            }
+
+
+        });
+
+
+        nameinput.value = '' //questo fa sì che una volta che clicchi sul button si elimini il value. Non si vede niente nell'input.
+        surnameinput.value = ''
+    }
+})
